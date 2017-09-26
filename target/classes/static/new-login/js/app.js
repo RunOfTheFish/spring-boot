b979df5763656141f97479a4ae901663abdbe14c
Su Lihao <sulihao@yingxinhuitong.com>
2017-09-26 16:05:12 +0800

    第6版

--- target/classes/static/new-login/js/app.js
+++ target/classes/static/new-login/js/app.js
@@ -10,7 +10,7 @@ $(function() {
       var $field = $(validity.field);
       var $group = $field.closest('.am-input-group');
       var $alert = $group.find('.am-alert');
-      // 使用自定义的提示信息 �? 插件内置的提示信�?
+      // 使用自定义的提示信息 或 插件内置的提示信息
       var msg = $field.data('validationMessage') || this.getValidationMessage(validity);
 
       if (!$alert.length) {
